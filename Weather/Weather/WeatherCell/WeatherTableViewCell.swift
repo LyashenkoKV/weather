58078da6574bdbffa3eee6b82e34999a865a7089
LyashenkoKV <Lyashenko@inbox.ru>
2023-02-14 20:40:10 +0300

    added XibCell

---
+++ Weather/Weather/WeatherCell/WeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherTableViewCell.swift
+//  Weather
+//
+//  Created by Konstantin Lyashenko on 14.02.2023.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
