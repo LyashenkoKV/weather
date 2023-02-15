b6987b2a266fb591249ea1e1248a375b64116a51
LyashenkoKV <Lyashenko@inbox.ru>
2023-02-15 18:37:52 +0300

    added delegate

--- Weather/WeatherProject/Controller/ViewController.swift
+++ Weather/WeatherProject/Controller/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.requestLocation()
+        
+        searchTextField.delegate = self
+        weatherManager.delegate = self
+        myTableView.delegate = self
     }
-
-
+    
+    @IBAction func currentLocationButton(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
+    @IBAction func searchButton(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    @IBAction func addDataButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    
+}
+
+// MARK: - WeatherManagerDelegate
+extension ViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        <#code#>
+    }
+    
+    func didFailWithError(error: Error) {
+        <#code#>
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    
+}
