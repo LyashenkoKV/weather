e388bd952e734757fe579cf3d2c99c593306b332
LyashenkoKV <Lyashenko@inbox.ru>
2023-02-15 17:48:04 +0300

    added WeatherData

---
+++ Weather/WeatherProject/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Weather
+//
+//  Created by Konstantin Lyashenko on 15.02.2023.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    let description: String
+}
