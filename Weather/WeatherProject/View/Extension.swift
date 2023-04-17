0a2657df230f9037ca9223c2e44cc6c9405e7bb9
LyashenkoKV <Lyashenko@inbox.ru>
2023-04-17 14:01:58 +0300

    add color gradient extension

---
+++ Weather/WeatherProject/View/Extension.swift
@@ -0,0 +1,48 @@
+//
+//  Extension.swift
+//  WeatherProject
+//
+//  Created by Konstantin Lyashenko on 06.04.2023.
+//
+
+import UIKit
+
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
+
+extension UIColor {
+    // hex sample: 0xf43737
+    convenience init(_ hex: Int, alpha: Double = 1.0) {
+        self.init(red: CGFloat((hex >> 16) & 0xFF) / 255.0, green: CGFloat((hex >> 8) & 0xFF) / 255.0, blue: CGFloat((hex) & 0xFF) / 255.0, alpha: CGFloat(255 * alpha) / 255)
+    }
+    
+    convenience init(_ hexString: String, alpha: Double = 1.0) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (1, 1, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(255 * alpha) / 255)
+    }
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1) {
+        self.init(red: (r / 255), green: (g / 255), blue: (b / 255), alpha: a)
+    }
+}
